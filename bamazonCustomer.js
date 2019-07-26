e2d6515897db7172d40699f6a8825331177df74f
Eddie Callejas <eddiecallejas@hotmail.com>
2019-07-26 09:47:38 -0500

    table update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,6 +5,14 @@ var table = new Table({
   head: ['Item ID', 'Item', 'Price', 'Qnty'],
   colWidths: [20, 40, 15, 15]
 });
+
+var FgBlue = "\x1b[34m";
+var FgWhite = "\x1b[0m";
+var FgCyan = "\x1b[36m";
+var FgGreen = "\x1b[32m";
+var FgMagenta = "\x1b[35m";
+
+
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -24,3 +32,14 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
+
+start_app();
+
+//Displays products in database table
+function start_app() {
+  connection.query('SELECT * FROM Products', function (err, res) {
+    // Display products and price to user pushed from DB into the CLI-table 
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].price.toFixed(2), res[i].stock_quantity])
+    }
+    console.log(table.toString());
--- schema.sql
+++ schema.sql
@@ -5,40 +5,40 @@ CREATE DATABASE bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
+  item_id INT (4) NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(45) NULL,
   department_name VARCHAR(45) NULL,
   price DECIMAL(10,2) NULL,
-  stock_stock_quantity INT NULL,
-  PRIMARY KEY (id)
+  stock_quantity INT NULL,
+  PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("computer", "electronics", 220.50, 10);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("2021", "computer", "electronics", 220.50, 10);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("radio", "electronics", 30.10, 120);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("2030", "radio", "electronics", 30.10, 120);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("tv", "electronics", 500.25, 75);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("3040", "tv", "electronics", 500.25, 75);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("nike", "shoes", 100.50, 100);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("4567", "nike", "shoes", 100.50, 100);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("playstation", "electronics", 400.10, 120);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("5876", "playstation", "electronics", 400.10, 120);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("reebok", "shoes", 100.25, 75);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("2134", "reebok", "shoes", 100.25, 75);
 
-INSERT INTO products (product_name, department_name,  price, stock_quantity)
-VALUES ("controller", "electronics", 29.50, 100);
+INSERT INTO products (item_id, product_name, department_name,  price, stock_quantity)
+VALUES ("8989", "controller", "electronics", 29.50, 100);
 
-INSERT INTO products (product_name, department_name,  price, stock_quantity)
-VALUES ("necklace", "jewelry", 420.10, 120);
+INSERT INTO products (item_id, product_name, department_name,  price, stock_quantity)
+VALUES ("9876", "necklace", "jewelry", 420.10, 120);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("ring", "jewelry", 200.25, 75);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("5467", "ring", "jewelry", 200.25, 75);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("watch", "jewelry", 299.25, 75);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("2345", "watch", "jewelry", 299.25, 75);
