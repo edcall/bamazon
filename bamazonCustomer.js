6feb8db6fd211d44744b87e643ee5bf25ab2de06
Eddie Callejas <eddiecallejas@hotmail.com>
2019-08-15 13:16:28 -0500

    update port

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,7 +23,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "password",
+  password: "Babyheri20!9",
   database: "bamazonDB"
 });
 
@@ -42,4 +42,87 @@ function start_app() {
     for (var i = 0; i < res.length; i++) {
       table.push([res[i].item_id, res[i].product_name, res[i].price.toFixed(2), res[i].stock_quantity])
     }
-    console.log(table.toString())
+    console.log(table.toString());
+ // Beginning user experience 
+ inquirer.prompt([{
+      
+  name: "choice",
+  type: "list",
+  message: "Use the arrow key to select your product",
+  
+  choices: function (value) {
+    var listArray = [];
+    for (var i = 0; i < res.length; i++) {
+      listArray.push(res[i].product_name);
+    }
+    return listArray;
+  }
+}, {
+  // User input
+  name: "quantity",
+  type: "input",
+  message: "Great, now that you have picked your product, how many would you like?  Please note the amount available in column labeled Qnty.",
+  validate: function (value) {
+    if (isNaN(value) == false) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}]).then(function (answer) {
+  // Gets product object user chose
+  for (var i = 0; i < res.length; i++) {
+    if (res[i].product_name == answer.choice) {
+      var selectedItem = res[i];
+    }
+  }
+  // Calculate remaining stock from purchase
+  var updatedInventory = parseInt(selectedItem.stock_quantity) - parseInt(answer.quantity);
+  var pSales = parseFloat(selectedItem.product_updated).toFixed(2);
+  //console.log (`PSale: ${pSales}`);
+  // If user does not pay attention to Qnty column and orders more than what is in stock, they will be asked if to make another purchase
+  if (selectedItem.stock_quantity < parseInt(answer.quantity)) {
+    console.log(`${FgCyan} Insufficient quantity! ${FgWhite}`);
+    repeat();
+  }
+  // If user wants to purchase an amount in stock, the price will be presented and remaining stock quantity updated in the mysql
+  else {
+
+    // Get total from new purchase, fetch current sales from table and add together.
+    var Total = (parseFloat(answer.quantity) * selectedItem.price).toFixed(2);
+    //console.log(`Total: ${Total}`);
+    //console.log (parseFloat(Total) + parseFloat(pSales)).toFixed(2);
+    var pTotal = (parseFloat(Total) + parseFloat(pSales)).toFixed(2);
+    //console.log(selectedItem.product_updated);
+    var query = connection.query("UPDATE Products SET ?, ? WHERE ?", [{ stock_quantity: updatedInventory }, { product_updated: pTotal }, { item_id: selectedItem.item_id }], function (err, res) {
+      if (err) throw err;
+      console.log(`${FgCyan} Purchase successful! ${FgWhite}`);
+      console.log("Your total is $ " + FgGreen + Total);
+      repeat();
+    });
+  }
+
+}); // .then of inquirer prompt
+
+}); // first connection.query of the database
+
+} 
+
+//Function used to make the experience of the CLI mode like a program. Provides an exit choice to the user.
+function repeat() {
+inquirer.prompt({
+// Ask user if he wants to purchase another item
+name: "repurchase",
+type: "list",
+choices: ["Yes", "No"],
+message: "Would you like to purchase another item?"
+}).then(function (answer) {
+if (answer.repurchase == "Yes") {
+  time_2Buy();
+}
+else {
+  console.log(`${FgMagenta} Thanks for being a smart shopper, please visit us again! ${FgWhite}`)
+  connection.end();
+}
+});
+}
